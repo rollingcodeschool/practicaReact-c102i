1e70983f54c272c99d4e6e2e87d295a428fa412b
Emilse Arias <emilsearias@gmail.com>
2024-08-19 10:09:28 -0300

    agregamos dos props desde app a el componente Informacion

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Contador from "./components/Contador";
 
 function App() {
   // aqui agrego la logica de un componente
+  const anio = 2024;
 
   return (
     <>
     <section className="container my-5">
       <h1 className="text-center">Practica inicial de React</h1>
       {/* aqui voy agregar un componente */}
-      <Informacion/>
+      <Informacion comision='c102i' anioActual={anio} />
       {/* <Informacion></Informacion> */}
     </section>
     <Contador></Contador>
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,10 +1,11 @@
 import Teoria from "./Teoria";
 
-const Informacion = () => {
+const Informacion = (props) => {
     return (
         <article className="mt-4">
-           <h2>Introducción</h2>     
+           <h2>Introducción comision: {props.comision}</h2>     
            <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoque en la creación de aplicaciones web de una sola página <b>SPA</b> y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <span className="palabraResaltada">DOM virtual</span>. React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
+           <p>Año de actualización: {props.anioActual}</p>
            <Teoria></Teoria>
         </article>
     );
